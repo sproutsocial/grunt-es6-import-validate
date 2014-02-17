3036aa08a97926a809bf9c5014f44884479d20a1
Jacob Gable <jacob.gable@gmail.com>
2014-02-17 14:53:55 -0600

    Add support for custom module naming
    - Introduce custom ES6ModuleFile class with getModuleName override
    - Allow passing moduleName function as option
    - Update to es6-import-validate 0.0.3
    - Bump version for publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,18 @@ module.exports = function (grunt) {
 					src: ['foo.js', 'bar.js']
 				}]
 			},
+			renamed: {
+				options: {
+					moduleName: function (filePath) {
+						return 'appkit/' + filePath;
+					}
+				},
+				files: [{
+					expand: true,
+					cwd: path.join(__dirname, 'tests', 'fixtures', 'renamed'),
+					src: ['foo.js', 'bar.js']
+				}]
+			},
 			fail: {
 				files: [{
 					expand: true,
